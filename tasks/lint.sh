2b05ab46da4bcd82f23f2b03f6a6407e7f48743c
Daniil Galtsev <galtsev.daniil@gmail.com>
2021-02-17 19:11:55 +0300

    Removed unsafe tornado dependency. Modified lint.sh to echo per task results.

--- tasks/lint.sh
+++ tasks/lint.sh
@@ -3,38 +3,50 @@
 set -uo pipefail
 set +e
 
-FAILURE=false
+for idx in {0..6}
+do
+    FAILURES[idx]=false
+done
 
 path=$(dirname "$0")
 cd "$path/.." || { echo "cd failed"; exit 1; }
 
 echo "Running safety check"
-safety check -r requirements/requirements.txt -r requirements/requirements-dev.txt || FAILURE=true
+safety check -r requirements/requirements.txt -r requirements/requirements-dev.txt || FAILURES[0]=true
+echo "safety failure: ${FAILURES[0]}"
 
 echo "Running pylint"
-pylint imagenet_training || FAILURE=true
+pylint imagenet_training || FAILURES[1]=true
+echo "pylint failure: ${FAILURES[1]}"
 
 echo "Running pycodestyle"
-pycodestyle imagenet_training || FAILURE=true
+pycodestyle imagenet_training || FAILURES[2]=true
+echo "pycodestyle failure: ${FAILURES[2]}"
 
 echo "Running pydocstyle"
-pydocstyle imagenet_training || FAILURE=true
+pydocstyle imagenet_training || FAILURES[3]=true
+echo "pydocstyle failure: ${FAILURES[3]}"
 
 echo "Running mypy"
-mypy imagenet_training || FAILURE=true
+mypy imagenet_training || FAILURES[4]=true
+echo "mypy failure: ${FAILURES[4]}"
 
 echo "Running bandit"
-bandit -ll -r imagenet_training || FAILURE=true
+bandit -ll -r imagenet_training || FAILURES[5]=true
+echo "bandit failure: ${FAILURES[5]}"
 
 echo "shellcheck"
-shellcheck tasks/*.sh || FAILURE=true
+shellcheck tasks/*.sh || FAILURES[6]=true
+echo "shellcheck failure: ${FAILURES[6]}"
 
-sleep 10
 
-if [ "$FAILURE" = true ]; then
-  echo "Linting failed"
-  exit 1
-fi
+for failure in "${FAILURES[@]}"
+do
+    if [ "$failure" = true ]; then
+        echo "Linting failed"
+        exit 1
+    fi
+done
 
 echo "Linting passed"
 exit 0
